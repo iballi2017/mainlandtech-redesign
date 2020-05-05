34aa12225831c722651ede5d1c6239d018d09e3e
iballi2017 <iballi2017@gmail.com>
2020-05-05 09:36:52 +0100

    05/05/2020 commit

--- js/main.js
+++ js/main.js
@@ -72,6 +72,7 @@ sectionOneObserver.observe(sectionOne)
 
 
 
+// animate Service list on "what we do section"
 const serviceList = document.querySelectorAll(".service-list");
 const options = {
     rootMargin: "550px 0px 0px 0px"
@@ -83,7 +84,7 @@ const observer = new IntersectionObserver((entries, observer) => {
             return
         }
         entry.target.classList.remove("service-list")
-        entry.target.classList.add("servise-list-intersect")
+        entry.target.classList.add("service-list-intersect")
     })
 }, options)
 
@@ -91,4 +92,69 @@ serviceList.forEach(serviceItem => {
     observer.observe(serviceItem);
 })
 
-console.log("Hello Programmers!")
+
+
+// animate hero texts and butoon
+const heroTexts = document.querySelector(".hero-texts")
+const heroBtn = document.querySelector(".hero-btn")
+const heroObserverOption = {
+    rootMargin: "-900px 0px 0px 0px"
+}
+
+const heroObserver = new IntersectionObserver((entries, heroObserver) => {
+    entries.forEach(entry => {
+        // console.log(entry.target)
+        if (!entry.isIntersecting) {
+            entry.target.classList.remove("hero-texts")
+            entry.target.classList.remove("hero-btn")
+            entry.target.classList.add("hero-texts-in")
+            entry.target.classList.add("hero-btn-in")
+        }
+    })
+}, heroObserverOption)
+
+heroObserver.observe(heroTexts)
+heroObserver.observe(heroBtn)
+
+
+//animate team-member-card
+const teamCards = document.querySelectorAll(".team-member-card")
+const teamObserverOption = {
+    rootMargin: "550px 0px 0px 0px"
+}
+const teamObserver = new IntersectionObserver((entries, teamObserver) => {
+    entries.forEach(entry => {
+        // console.log(entry.target)
+        if (!entry.isIntersecting) {
+            return;
+        }
+        entry.target.classList.remove("team-member-card-animate")
+        entry.target.classList.add("team-member-card-animate-alt")
+    })
+}, teamObserverOption)
+teamCards.forEach(teamCard => {
+    teamObserver.observe(teamCard)
+})
+
+// animate about-us texts and image
+const manComputerImg = document.querySelector(".man-computer-img")
+const aboutUsTexts = document.querySelector(".about-us-texts")
+const aboutUsObserverOptions = {
+    rootMargin: "550px 0px 0px 0px"
+}
+
+const aboutUsObserver = new IntersectionObserver((entries, aboutUsObserver) => {
+    entries.forEach(entry => {
+        // console.log(entry.target)
+        if (!entry.isIntersecting) {
+            return;
+        }
+        entry.target.classList.remove("man-computer-img")
+        entry.target.classList.add("about-us-texts")
+        entry.target.classList.add("about-us-intersect")
+    })
+}, aboutUsObserverOptions)
+
+aboutUsObserver.observe(manComputerImg)
+aboutUsObserver.observe(aboutUsTexts)
+
